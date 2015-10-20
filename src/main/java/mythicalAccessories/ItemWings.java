b41c7ca5f3ff26f88350b3a746b70af99718d426
Scotty <scottyaa63@gmail.com>
2015-10-19 22:13:55 -0700

    Wings Added

---
+++ src/main/java/mythicalAccessories/ItemWings.java
@@ -0,0 +1,13 @@
+package mythicalAccessories;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+
+public class ItemWings extends ItemArmor {
+
+	public ItemWings(ArmorMaterial material, int renderIndex, int armorType) {
+		super(material, renderIndex, armorType);
+		
+	}
+
+}

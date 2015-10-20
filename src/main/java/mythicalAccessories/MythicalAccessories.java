bf6d2a412976d113dd30c670e3effb31ce1bbdce
Scotty <scottyaa63@gmail.com>
2015-10-19 21:41:52 -0700

    MythicalAccessories Added

---
+++ src/main/java/mythicalAccessories/MythicalAccessories.java
@@ -0,0 +1,23 @@
+package mythicalAccessories;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+import com.example.examplemod.ExampleMod;
+
+@Mod(modid = MythicalAccessories.MODID, version = MythicalAccessories.VERSION)
+public class MythicalAccessories {
+
+	public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+	
+  
+    //Initialize everything
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        
+    }
+}

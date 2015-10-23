3aaa7cc90a2fa6613e912e30bb4e0a29e8fa6034
dhalen8 <daniellehalen@rocketmail.com>
2015-10-22 19:56:41 -0700

    comments for new world

--- src/main/java/newWorlds/NewWorlds.java
+++ src/main/java/newWorlds/NewWorlds.java
@@ -2,4 +2,6 @@ package newWorlds;
 
 public class NewWorlds {
 //The World A New
+	//That is the name btw :3
+	//I really hope we can make another mod file i DO NOT  want to go through that again ;~;
 }

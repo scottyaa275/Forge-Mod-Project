f6004814e10897cf5b920b8035bb2e30da439aef
Scotty <scottyaa63@gmail.com>
2015-10-22 20:00:07 -0700

    TWAN Update 1.0.001
    +Added The World A New

--- src/main/java/newWorlds/NewWorlds.java
+++ src/main/java/newWorlds/NewWorlds.java
@@ -1,5 +1,20 @@
 package newWorlds;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = NewWorlds.MODID, version = NewWorlds.VERSION, name = NewWorlds.NAME)
 public class NewWorlds {
-//The World A New
+	//The World A New
+	
+	public static final String NAME = "The World A New";
+	public static final String MODID = "newWorlds";
+	public static final String VERSION = "1.8";
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
 }

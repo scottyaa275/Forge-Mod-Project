139423f1cc1a654d3d539c83faf78bdf5e5dec72
Scotty <scottyaa63@gmail.com>
2015-10-23 19:50:19 -0700

    Update TWAN

--- src/main/java/newWorlds/NewWorlds.java
+++ src/main/java/newWorlds/NewWorlds.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockGrass;
 import net.minecraft.block.BlockLog;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -21,9 +22,10 @@ public class NewWorlds {
 	public static final String NAME = "The World A New";
 	public static final String MODID = "newWorlds";
 	public static final String VERSION = "1.8";
+	//1.0.010
 	
-	public static Block leaves;
-	public static Block log;
+	public static Block betterLeaves;
+	public static Block betterLog;
 	
 	/**A better version of the normal grass block*/
 	public static Block betterGrass;
@@ -42,7 +44,10 @@ public class NewWorlds {
 		GameRegistry.registerBlock(betterDirt, "better_dirt");
 		blockRenderRegister(betterDirt, 0, "better_dirt");
 		
-		log = new BlockLogNew();
+		betterLog = new BlockLogNew().setHardness(1.0f).setUnlocalizedName("better_log");
+		GameRegistry.registerBlock(betterLog, "better_log");
+		blockRenderRegister(betterLog, 0, "better_log");
+		
 	}
 	
 	
